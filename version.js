edfb46776d57fc32eb696410d3efcfe95760ec05
Desenvolve Fácil <suporte@desenvolvefacil.com.br>
2025-10-13 21:11:13 -0300

    Create version.js

---
+++ version.js
@@ -0,0 +1,6 @@
+{
+    "latest_version": "0.0.1",
+    "release_notes": "Modulos em fase de testes",
+    "mandatory": false,
+    "download_url" : "https://github.com/desenvolvefacil/dfboletomercadopagowhmcsbrasil/"
+}
