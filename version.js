f5f2a028019237c4a9df80189a2fbe28ce2bfcb3
Desenvolve Fácil <suporte@desenvolvefacil.com.br>
2025-10-22 17:31:07 -0300

    Update version.js

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 {
-    "latest_version": "2.0.0",
+    "latest_version": "2.0.1",
     "release_notes": "Reembolso realizado no email do cliente",
     "mandatory": false,
     "download_url" : "https://github.com/desenvolvefacil/dfboletomercadopagowhmcsbrasil/"
